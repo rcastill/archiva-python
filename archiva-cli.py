d183af843a113161b7fe3c977b09c385ef5a5af4
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2019-05-23 09:37:48 -0400

    Add support for custom instructions

--- archiva-cli.py
+++ archiva-cli.py
@@ -2,6 +2,37 @@ import archiva
 import argparse
 import os
 import json
+import sys
+import pprint
+
+def instruction_is_valid(ins):
+	return\
+		ins != "i" or\
+		not ins.startswith("versionsList:") or\
+		not ins.startswith("downloadInfos:")
+
+def instruction_execute(session, ins):
+	def get_group_and_name(group_p_name):
+		group_p_name_split = group_p_name.split(".")
+		return\
+			".".join(group_p_name_split[:-1]),\
+			group_p_name_split[-1]
+
+	if ins.startswith("versionsList:"):
+		group_p_name = ins.split(":")[1]
+		return session.get_versions_list(
+			*get_group_and_name(group_p_name))
+
+	elif ins.startswith("downloadInfos:"):
+		_, group_p_name, version = ins.split(":")
+		return session.get_download_infos(
+			*get_group_and_name(group_p_name),
+			version)
+
+	else:
+		print(f"Invalid instruction: {ins}",
+			file=sys.stderr)
+
 
 def get_args():
 	parser = argparse.ArgumentParser()
@@ -21,8 +52,21 @@ def get_args():
 	parser.add_argument("-p", "--password",
 		help="archiva password (default=), also taken from $ARCHIVA_PWD",
 		default="")
+	parser.add_argument("-x", "--execute",
+		help="execute instruction (default=i(nteractive))",
+		default="i")
 
 	args = parser.parse_args()
+
+	if not instruction_is_valid(args.execute):
+		print(f"Invalid instruction: {args.execute}",
+			file=sys.stderr, end=", ")
+		print(f"Must be one of:", file=sys.stderr)
+		print(f"\t* i", file=sys.stderr)
+		print(f"\t* versionsList:{{group}}.{{name}}",
+			file=sys.stderr)
+		print(f"\t* downloadInfos:{{group}}.{{name}}:{{version}}",
+			file=sys.stderr)
 
 	if args.user == "guest":
 		args.user = os.getenv("ARCHIVA_USR", "guest")
@@ -31,7 +75,7 @@ def get_args():
 
 	return args
 
-if __name__ == "__main__":
+def main():
 	args = get_args()
 	logger = archiva.Logger(args.verbose_level)
 
@@ -39,11 +83,23 @@ if __name__ == "__main__":
 		s = archiva.Session(args.host,
 			args.user, args.password,
 			set_referer=args.set_referer, logger=logger)
+		pp = pprint.PrettyPrinter(indent=2)
 		with s:
-			vlist = s.get_versions_list("com.xompass.edge", "Printer")
-			logger.i(vlist)
+			if args.execute == "i":
+				while True:
+					ins = input("ins> ")
+					if ins == "q":
+						return
+					pp.pprint(instruction_execute(s, ins))
+			else:
+				pp.pprint(instruction_execute(s, args.execute))
+
 	except json.decoder.JSONDecodeError as e:
 		logger.e(f"could not decode: {e.doc}")
 	except archiva.ErrorResponse as e:
 		logger.e(e)
 
+
+
+if __name__ == "__main__":
+	main()
